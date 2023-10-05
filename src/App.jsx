754d106ffae2706d284944b54ccc40373e39792a
Faahm <fatmahinay99@gmail.com>
2023-10-05 19:05:58 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <Post />
+      <div className="flex items-center gap-3 mt-3">
+        {Array.from({ length: 5 }).map((value, index) => {
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <div className="p-2 border font-bold hover:cursor-pointer">
+              {index + 1}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+const Post = () => {
+  return (
+    <div className="text-center p-4 bh-white border border-gray-400/30 w-[250px]">
+      <h4>Title</h4>
+      <h4>Body</h4>
+    </div>
+  );
+};
+
+export default Post;
